c3a88b8ba4f8fb29f8fb460d53b7069934a0b31f
Catherine J <cjarocki@gmail.com>
2017-02-06 21:02:25 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-  $('#button').on('click', function(){
-		$('#content').css({'display': 'block'});
-    $('#button').css({'display': 'none'});
-  });
-  $('#button').hover(function(){
-    $(this).css('cursor', 'pointer');
-  });
-});
